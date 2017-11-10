8c12053e8aa0e69abd22e5ed059a41a428158030
sergey19941201 <koshelnick1994@mail.ru>
2017-11-11 01:58:09 +0200

    Timer added

---
+++ Povaga/Activities/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using System.Timers;
+
+namespace Povaga
+{
+    [Activity(Label = "Povaga", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, Theme = "@android:style/Theme.Black.NoTitleBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Timer timer = new Timer(3000);
+            timer.Elapsed += delegate
+            {
+                timer.Enabled = false;
+            };
+            timer.Enabled = true;
+        }
+    }
+}
+
