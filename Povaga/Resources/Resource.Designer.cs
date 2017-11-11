b18f0d20779605c15e8d645b27277529edd4584e
sergey19941201 <koshelnick1994@mail.ru>
2017-11-11 02:28:03 +0200

    gh

--- Povaga/Resources/Resource.Designer.cs
+++ Povaga/Resources/Resource.Designer.cs
@@ -67,10 +67,22 @@ namespace Povaga
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int imageView1 = 2131034112;
+			public const int button1 = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int button2 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int button3 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button4 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int button5 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView1 = 2131034117;
 			
 			static Id()
 			{
